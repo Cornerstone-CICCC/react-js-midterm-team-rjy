e2c94e3168254690e26df49d0bdd006d3c5b2957
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2026-01-12 12:01:56 -0800

    initial_setup

---
+++ backend/dist/server.js
@@ -0,0 +1,57 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+// Create server
+const app = (0, express_1.default)();
+// Middleware
+app.use((0, cors_1.default)({
+    origin: "http://localhost:4321",
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
+if (!process.env.COOKIE_PRIMARY_KEY || !process.env.COOKIE_SECONDARY_KEY) {
+    throw new Error("Missing cookie keys!");
+}
+app.use((0, cookie_session_1.default)({
+    name: "session",
+    keys: [process.env.COOKIE_PRIMARY_KEY, process.env.COOKIE_SECONDARY_KEY],
+    maxAge: 25 * 60 * 1000,
+    sameSite: "lax",
+    secure: false,
+}));
+app.use(express_1.default.json());
+// Routes
+// Create HTTP server and attach SocketIO
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:4321",
+        methods: ["GET", "POST"],
+    },
+});
+// Connect to MongoDB and start server
+const MONGO_URI = process.env.DATABASE_URL;
+mongoose_1.default
+    .connect(MONGO_URI, { dbName: "chatting_app" })
+    .then(() => {
+    console.log("Connected to MongoDB database");
+    // Start Socket.IO
+    // Start the server
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+})
+    .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
---
+++ backend/src/server.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Create server
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:4321",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+if (!process.env.COOKIE_PRIMARY_KEY || !process.env.COOKIE_SECONDARY_KEY) {
+  throw new Error("Missing cookie keys!");
+}
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_PRIMARY_KEY!, process.env.COOKIE_SECONDARY_KEY!],
+    maxAge: 25 * 60 * 1000,
+    sameSite: "lax",
+    secure: false,
+  })
+);
+
+app.use(express.json());
+
+// Routes
+
+// Create HTTP server and attach SocketIO
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4321",
+    methods: ["GET", "POST"],
+  },
+});
+
+// Connect to MongoDB and start server
+const MONGO_URI = process.env.DATABASE_URL!;
+mongoose
+  .connect(MONGO_URI, { dbName: "chatting_app" })
+  .then(() => {
+    console.log("Connected to MongoDB database");
+
+    // Start Socket.IO
+
+
+    // Start the server
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
